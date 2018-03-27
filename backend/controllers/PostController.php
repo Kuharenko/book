38e01c0fbaa627a89ac6e28da3d603035c2cac69
kuharenko <kuharenko.leha94@gmail.com>
2018-03-27 20:57:07 +0300

    изменил домен

--- backend/controllers/PostController.php
+++ backend/controllers/PostController.php
@@ -42,7 +42,7 @@ class PostController extends ActiveController
             [
                 'class' => Cors::className(),
                 'cors' => [
-                    'Origin' => ['*'],
+                    'Origin' => ['http://kuharenko.xyz/'],
                     'Access-Control-Request-Method' => ['GET'],
                 ],
             ],
