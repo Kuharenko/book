8f78baf22ce552851d38f1a7b2da8d4766d7c4fc
kuharenko <kuharenko.leha94@gmail.com>
2018-04-25 15:12:29 +0300

    обновил миграции

--- console/migrations/m180402_073751_progress_table.php
+++ console/migrations/m180402_073751_progress_table.php
@@ -22,7 +22,7 @@ class m180402_073751_progress_table extends Migration
 
         $this->addForeignKey('progress_user_id', 'progress', 'user_id', 'user', 'id');
         $this->addForeignKey('progress_material_id', 'progress', 'material_id', 'material', 'id');
-        $this->addForeignKey('progress_task_id', 'progress', 'task_id', 'tests', 'id');
+
     }
 
     /**
